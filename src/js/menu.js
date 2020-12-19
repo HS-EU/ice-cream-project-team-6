89a6192dd5d85d5e82182bb99cba12aa51c30832
Nataliia Byshovets <natashabysh11@gmail.com>
2020-12-19 22:53:55 +0200

    fixed minor issues

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,29 +1,49 @@
 (() => {
-  const menuBtnOpen = document.querySelector('[menu-button-open]');
-  const menuBtnClose = document.querySelector('[menu-button-close]');
-  const mobileMenuRef = document.querySelector('[data-menu]');
+  const refs = {
+    openBtn: document.querySelector('[menu-button-open]'),
+    closeBtn: document.querySelector('[menu-button-close]'),
+    menu: document.querySelector('[data-menu]'),
+    navLinks: document.getElementsByClassName('site-nav-link'),
+  };
 
-  menuBtnOpen.addEventListener('click', () => {
-    const expanded =
-      menuBtnOpen.getAttribute('aria-expanded') === 'true' || false;
-    const expanded2 =
-      menuBtnClose.getAttribute('aria-expanded') === 'false' || true;
+  refs.openBtn.addEventListener('click', toggleModal);
+  refs.closeBtn.addEventListener('click', toggleModal);
+  [...refs.navLinks].forEach(elem =>
+    elem.addEventListener('click', toggleModal),
+  );
 
-    menuBtnClose.classList.toggle('is-open');
-    menuBtnOpen.classList.toggle('is-open');
-
-    mobileMenuRef.classList.toggle('is-open');
-  });
-
-  menuBtnClose.addEventListener('click', () => {
-    const expanded =
-      menuBtnOpen.getAttribute('aria-expanded') === 'true' || false;
-    const expanded2 =
-      menuBtnClose.getAttribute('aria-expanded') === 'false' || true;
-
-    menuBtnClose.classList.toggle('is-open');
-    menuBtnOpen.classList.toggle('is-open');
-
-    mobileMenuRef.classList.toggle('is-open');
-  });
+  function toggleModal() {
+    refs.menu.classList.toggle('is-open');
+  }
 })();
+
+// (() => {
+//   const menuBtnOpen = document.querySelector('[menu-button-open]');
+//   const menuBtnClose = document.querySelector('[menu-button-close]');
+//   const mobileMenuRef = document.querySelector('[data-menu]');
+//   const references = document.getElementsByClassName('site-nav-link');
+
+//   menuBtnOpen.addEventListener('click', () => {
+//     const expanded =
+//       menuBtnOpen.getAttribute('aria-expanded') === 'true' || false;
+//     const expanded2 =
+//       menuBtnClose.getAttribute('aria-expanded') === 'false' || true;
+
+//     menuBtnClose.classList.toggle('is-open');
+//     menuBtnOpen.classList.toggle('is-open');
+
+//     mobileMenuRef.classList.toggle('is-open');
+//   });
+
+//   menuBtnClose.addEventListener('click', () => {
+//     const expanded =
+//       menuBtnOpen.getAttribute('aria-expanded') === 'true' || false;
+//     const expanded2 =
+//       menuBtnClose.getAttribute('aria-expanded') === 'false' || true;
+
+//     menuBtnClose.classList.toggle('is-open');
+//     menuBtnOpen.classList.toggle('is-open');
+
+//     mobileMenuRef.classList.toggle('is-open');
+//   });
+// })();
