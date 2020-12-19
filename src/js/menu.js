6e39a8f47b5ffaf86e3f6aa788ef91c1014bb833
Nataliia Byshovets <natashabysh11@gmail.com>
2020-12-19 23:08:05 +0200

    update menu.js

--- src/js/menu.js
+++ src/js/menu.js
@@ -16,34 +16,3 @@
     refs.menu.classList.toggle('is-open');
   }
 })();
-
-// (() => {
-//   const menuBtnOpen = document.querySelector('[menu-button-open]');
-//   const menuBtnClose = document.querySelector('[menu-button-close]');
-//   const mobileMenuRef = document.querySelector('[data-menu]');
-//   const references = document.getElementsByClassName('site-nav-link');
-
-//   menuBtnOpen.addEventListener('click', () => {
-//     const expanded =
-//       menuBtnOpen.getAttribute('aria-expanded') === 'true' || false;
-//     const expanded2 =
-//       menuBtnClose.getAttribute('aria-expanded') === 'false' || true;
-
-//     menuBtnClose.classList.toggle('is-open');
-//     menuBtnOpen.classList.toggle('is-open');
-
-//     mobileMenuRef.classList.toggle('is-open');
-//   });
-
-//   menuBtnClose.addEventListener('click', () => {
-//     const expanded =
-//       menuBtnOpen.getAttribute('aria-expanded') === 'true' || false;
-//     const expanded2 =
-//       menuBtnClose.getAttribute('aria-expanded') === 'false' || true;
-
-//     menuBtnClose.classList.toggle('is-open');
-//     menuBtnOpen.classList.toggle('is-open');
-
-//     mobileMenuRef.classList.toggle('is-open');
-//   });
-// })();
