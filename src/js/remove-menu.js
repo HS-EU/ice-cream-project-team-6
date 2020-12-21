aff7ba9994137b2fb5fb3608727145f8a717d20a
Nataliia Byshovets <natashabysh11@gmail.com>
2020-12-21 11:24:27 +0200

    update remove-menu.js

--- src/js/remove-menu.js
+++ src/js/remove-menu.js
@@ -1,8 +1,7 @@
-$(document).mouseup(function (e){ 
-    var div = $(".mobile-menu-content"); 
-    if (!div.is(e.target) 
-        && div.has(e.target).length === 0) { 
-       $('.mobile-menu').removeClass('is-open');
-        $('.content-container-overlay').removeClass('is-open');
-    }
-  });
+$(document).mouseup(function (e) {
+  var div = $('.mobile-menu-content');
+  if (!div.is(e.target) && div.has(e.target).length === 0) {
+    $('.mobile-menu').removeClass('is-open');
+    $('.content-container-overlay').removeClass('is-open');
+  }
+});
